5e9d4949d91d0fdea7f3f6b9fa9bf43942e6db6f
wiktorpp <grazynapp65@gmail.com>
2021-05-28 20:43:36 +0200

    Added a preetier prompt

--- faketerminal.py
+++ faketerminal.py
@@ -3,11 +3,18 @@ import sys
 import traceback
 
 def bash():
+    username = "root"
+    prompt = f"\033[01;32m{username}@{'localhost'}\033[00m:\033[01;34m{os.getcwd()}\033[00m$ "
     while True:
-        command = input("$ ").split(" ")
+        command = input(prompt).split(" ")
         if command[0] == "exit": break
         try:
             output = globals()[command[0]](*command[1:])
+        except KeyError:
+            if command[0] == "":
+                continue
+            else:
+                print(f"{command[0]}: command not found")
         except:
             print(traceback.format_exc())
         else:
@@ -37,6 +44,8 @@ with Patcher() as patcher:
 
     ls = lambda path=None : "\n".join(os.listdir(path if path != None else "."))
 
+    def mount(path):
+        patcher.fs.add_real_directory(path)
 
     patcher.fs.create_file('/foo/bar', contents='test')
 
